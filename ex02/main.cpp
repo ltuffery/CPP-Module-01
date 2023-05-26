5a493f8f8b8647528fb378bb032904e1003ca0de
ltuffery <123221865+ltuffery@users.noreply.github.com>
2023-05-26 19:33:43 +0200

    finish ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main( void ) {
+	std::string string = "HI THIS IS BRAIN";
+	std::string *stringPTR = &string;
+	std::string &stringREF = string;
+	
+	std::cout << "'string' address is: " << &string << std::endl;
+	std::cout << "'stringPTR' address is: " << &stringPTR << std::endl;
+	std::cout << "'stringREF' address is: " << &stringREF << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "'string' value is: " << string << std::endl;
+	std::cout << "'stringPTR' value is: " << stringPTR << std::endl;
+	std::cout << "'stringREF' value is: " << stringREF << std::endl;
+}
