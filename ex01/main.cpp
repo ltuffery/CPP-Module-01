b890c018c4f5030418dbd6ae02da4f664db27d75
ltuffery <123221865+ltuffery@users.noreply.github.com>
2023-07-18 16:01:07 +0200

    protect horde

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,11 +1,29 @@
 #include "Zombie.hpp"
 
 int main( void ) {
-	Zombie *horde;
+	{
+		Zombie *horde;
 
-	horde = zombieHorde(3, "test");
-	for (int i = 0; i < 3; i++) {
-		horde[i].announce();
+		horde = zombieHorde(3, "test");
+		for (int i = 0; i < 3; i++) {
+			horde[i].announce();
+		}
+		delete [] horde;
 	}
-	delete [] horde;
+	{
+		Zombie *horde;
+
+		horde = zombieHorde(0, "test");
+		if (horde == NULL) {
+			std::cout << "C'est null" << std::endl;
+		}
+	}
+	{
+		Zombie *horde;
+
+		horde = zombieHorde(-5, "oui");
+		if (horde == NULL) {
+			std::cout << "C'est null" << std::endl;
+		}
+	}
 }
