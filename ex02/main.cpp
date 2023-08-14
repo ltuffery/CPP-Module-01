e4d332704c4e46dfe79e6f032bb2525f7343ca1a
ltuffery <123221865+ltuffery@users.noreply.github.com>
2023-08-14 09:44:57 +0200

    fix ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -7,12 +7,12 @@ int main( void ) {
 	std::string &stringREF = string;
 	
 	std::cout << "'string' address is: " << &string << std::endl;
-	std::cout << "'stringPTR' address is: " << &stringPTR << std::endl;
+	std::cout << "'stringPTR' address is: " << stringPTR << std::endl;
 	std::cout << "'stringREF' address is: " << &stringREF << std::endl;
 
 	std::cout << std::endl;
 
 	std::cout << "'string' value is: " << string << std::endl;
-	std::cout << "'stringPTR' value is: " << stringPTR << std::endl;
+	std::cout << "'stringPTR' value is: " << *stringPTR << std::endl;
 	std::cout << "'stringREF' value is: " << stringREF << std::endl;
 }
