9918ffabf6e1ef63cc34a76380458916a4fc21de
ltuffery <123221865+ltuffery@users.noreply.github.com>
2023-08-14 11:58:09 +0200

    fix sed prog boucke

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -18,18 +18,22 @@ void replace(const char * filename, std::string s1, std::string s2) {
 	if (file.is_open()) {
 		while (std::getline(file, line)) {
 			loc = line.find(s1);
+			if (loc == std::string::npos)
+				final += line;
 			while (loc != std::string::npos) {
 				start = line.substr(0, loc);
-				end = line.substr(loc + s1.length());
-				line = start + s2 + end;
+				line = line.substr(loc + s1.length());
+				final += start + s2;
 				loc = line.find(s1);
+				if (loc == std::string::npos)
+					final += line;
 			}
-			final += line;
+			final += "\n";
 		}
 		new_file.append(filename);
 		new_file.append(".replace");
 		outfile.open(new_file.c_str());
-		outfile << final << std::endl;
+		outfile << final;
 		outfile.close();
 		file.close();
 	}
@@ -40,5 +44,9 @@ int main(int ac, char **av) {
 		std::cout << "invalid number args" << std::endl;
 		return 1;
 	}
-	replace(av[1], av[2], av[3]);
+	std::string s1 = av[2];
+	if (s1.empty())
+		std::cout << "arg 2 cannot be empty" << std::endl;
+	else
+		replace(av[1], s1, av[3]);
 }
